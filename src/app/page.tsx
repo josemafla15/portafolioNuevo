40c58f26536e2c8887d46c16147ec674617e2fac
Jose Manuel Mafla Mafla <jose.maflamun@campusucc.edu.co>
2025-03-15 14:13:15 -0500

    normalizacion saltos de linea

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-
-
 export default function Home() {
   return (
-   <main className= 'flex items-center justify-center w-screen h-screen bg-cyan-700'>
-    <h1 className='text-cyan600 text-4xl font-extra'>Jose Manuel Mafla Muñoz</h1>
-   </main>
+    <main className="flex items-center justify-center w-screen h-screen bg-background text-text">
+      <h1 className="text-primary text-4xl font-extrabold">Jose Manuel Mafla Muñoz</h1>
+    </main>
   );
 }
+
